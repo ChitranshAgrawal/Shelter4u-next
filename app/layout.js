863f19f968445eccdd090d9938705545bcdb6abd
ChitranshAgrawal <chitranshaga.2345@gmail.com>
2025-06-20 15:49:23 +0530

    new commit 5 - 20 june

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,8 @@
 import "./globals.css";
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import Header from "./Components/Header.jsx";
 import Footer from "./footer/page.jsx"; 
 
