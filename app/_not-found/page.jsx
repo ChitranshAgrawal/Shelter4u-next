62f115ef84aeabc8aa92969d192833653083b00c
ChitranshAgrawal <chitranshaga.2345@gmail.com>
2025-06-21 14:58:25 +0530

    git push 4

---
+++ app/_not-found/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Footer from "../footer/page.jsx";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <Footer/>
+    </div>
+  );
+}
+
