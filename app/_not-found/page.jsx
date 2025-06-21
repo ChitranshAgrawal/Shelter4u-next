05667f476e6d0310b2e08b4750fc4b23e6b15cf3
ChitranshAgrawal <chitranshaga.2345@gmail.com>
2025-06-21 15:02:10 +0530

    git push 4

--- app/_not-found/page.jsx
+++ app/_not-found/page.jsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import Footer from "../footer/page.jsx";
+import dynamic from "next/dynamic";
+
+const Footer = dynamic(() => import("../footer/page.jsx"), { ssr: false });
 
 export default function NotFound() {
   return (
     <div>
       <h1>Page Not Found</h1>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
-
