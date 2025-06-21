987bbfd586cf48326595b56682c6074ad1f955ae
ChitranshAgrawal <chitranshaga.2345@gmail.com>
2025-06-21 12:45:18 +0530

    git push

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,12 +9,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 const eslintConfig = [...compat.extends("next/core-web-vitals")];
-=======
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
->>>>>>> 7e66a0224e76de08ccb94ba0ef3fd5a088c1cb03
+// =======
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+// >>>>>>> 7e66a0224e76de08ccb94ba0ef3fd5a088c1cb03
 
 export default eslintConfig;
